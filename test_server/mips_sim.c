ebb75d6fe1076339b4859590ce81f47e801f2319
GangHyeok Lee <rkdgur5381@gmail.com>
2018-11-14 11:10:16 +0900

    decode의 control unit만
    Signed-off-by: GangHyeok Lee <rkdgur5381@gmail.com>

--- test_server/mips_sim.c
+++ test_server/mips_sim.c
@@ -9,6 +9,7 @@ long long cycles;
 
 // registers
 int regs[32];
+int * return_address;
 
 // program counter
 int pc;
@@ -19,16 +20,49 @@ int pc;
 int inst_mem[INST_MEM_SIZE]; //instruction memory
 int data_mem[DATA_MEM_SIZE]; //data memory
 
+//Control Unit
+typedef struct Execution{
+	char Regdst;
+	char ALUOp1;
+	char ALUOp0;
+	char ALUsrc;
+} Execution;
+
+typedef struct Memory {
+	char Branch;
+	char MemRead;
+	char MemWrite;
+} Memory;
+
+typedef struct WriteBack{
+	char RegWrite;
+	char MemtoReg;
+} WriteBack;
+
+typedef struct Control_Unit{
+	Execution EX;
+	Memory M;
+	WriteBack WB;
+	char jump;
+	char link;
+} Control_Unit;
+
+Control_Unit control;
+
+
 //misc. function
 void init();
 void fetch(); //regs[31] = LSB, regs[0] = MSB
+void decode();
+
+
+//sub_function
+void make_decimal(int * decimal, int start, int size); //decode instruction to decimal number
 
 //main
 int main()
 {
 	char done=FALSE;
-
-	char control_unit[9]; //regdst, ALUsrc, Memto-Reg, Reg-Write, Mem-Read, Branch, ALUOp1, ALUOp0
 
 	init();
 	while(!done)
@@ -91,8 +125,6 @@ void init()
 }
 
 
-
-
 //functions
 void fetch() //read address and make it bit
 {
@@ -111,13 +143,143 @@ void fetch() //read address and make it bit
 
 void decode()
 {
-	int * opcode = NULL;
-	opcode = (int*)calloc(7, sizeof(int));
+	int opcode = 0, jump_address = 0;
 
-	for (int i = 0; i < 7; i++) //copy opcode
+	make_decimal(&opcode, 0, 6); //decode opcode
+
+	switch (opcode)
 	{
-		opcode[i] = regs[i];
+	case 0: //R-format
+		control.EX.Regdst = 1;
+		control.EX.ALUOp1 = 1;
+		control.EX.ALUOp0 = 0;
+		control.EX.ALUsrc = 0;
+		control.M.Branch = 0;
+		control.M.MemRead = 1;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 1;
+		control.WB.MemtoReg = 0;
+		control.jump = 0;
+		control.link = 0;
+		break;
+	case 2: //Jump
+		control.EX.Regdst = 'x';
+		control.EX.ALUOp1 = 'x';
+		control.EX.ALUOp0 = 'x';
+		control.EX.ALUsrc = 'x';
+		control.M.Branch = 0;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 0;
+		control.WB.MemtoReg = 'x';
+		control.jump = 1;
+		control.link = 0;
+		break;
+	case 3: //Jal
+		control.EX.Regdst = 'x';
+		control.EX.ALUOp1 = 'x';
+		control.EX.ALUOp0 = 'x';
+		control.EX.ALUsrc = 'x';
+		control.M.Branch = 0;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 1;
+		control.WB.MemtoReg = 'x';
+		control.jump = 1;
+		control.link = 1;
+		break;
+	case 4: //beq
+		control.EX.Regdst = 'x';
+		control.EX.ALUOp1 = 0;
+		control.EX.ALUOp0 = 1;
+		control.EX.ALUsrc = 0;
+		control.M.Branch = 1;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 0;
+		control.WB.MemtoReg = 'x';
+		control.jump = 0;
+		control.link = 0;
+		break;
+	case 8: //add immediate
+		control.EX.Regdst = 0;
+		control.EX.ALUOp1 = 0;
+		control.EX.ALUOp0 = 0;
+		control.EX.ALUsrc = 1;
+		control.M.Branch = 0;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 1;
+		control.WB.MemtoReg = 0;
+		control.jump = 0;
+		control.link = 0;
+		break;
+	case 10: //set less than immediate
+		control.EX.Regdst = 0;
+		control.EX.ALUOp1 = 0;
+		control.EX.ALUOp0 = 0;
+		control.EX.ALUsrc = 1;
+		control.M.Branch = 0;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 1;
+		control.WB.MemtoReg = 0;
+		control.jump = 0;
+		control.link = 0;
+		break;
+	case 35: //load word
+		control.EX.Regdst = 0;
+		control.EX.ALUOp1 = 0;
+		control.EX.ALUOp0 = 0;
+		control.EX.ALUsrc = 1;
+		control.M.Branch = 0;
+		control.M.MemRead = 1;
+		control.M.MemWrite = 0;
+		control.WB.RegWrite = 1;
+		control.WB.MemtoReg = 1;
+		control.jump = 0;
+		control.link = 0;
+		break;
+	case 43: //store word
+		control.EX.Regdst = 'x';
+		control.EX.ALUOp1 = 0;
+		control.EX.ALUOp0 = 0;
+		control.EX.ALUsrc = 1;
+		control.M.Branch = 0;
+		control.M.MemRead = 0;
+		control.M.MemWrite = 1;
+		control.WB.RegWrite = 0;
+		control.WB.MemtoReg = 'x';
+		control.jump = 0;
+		control.link = 0;
+		break;
+	}
+
+	if (control.jump && !(control.link)) //jump execution
+	{
+		make_decimal(&jump_address, 6, 26); //decode jump address
+		jump_address << 2;
 	}
 
 
+}
+
+void make_decimal(int * decimal, int start, int size) //decode instruction to decimal number
+{
+	int i;
+
+	for (i = start; i < size; i++)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			if (regs[i])
+			{
+				(*decimal)++;
+			}
+			(*decimal) *= 2;
+		}
+
+	}
+
+	return;
 }
