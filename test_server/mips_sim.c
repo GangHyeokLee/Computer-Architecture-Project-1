69bc9eb1e63ac46f02d9f6d0ee00c63c4f8deed5
GangHyeok Lee <rkdgur5381@gmail.com>
2018-11-15 15:10:51 +0900

    Decode 다 만들었다
    Signed-off-by: GangHyeok Lee <rkdgur5381@gmail.com>

--- test_server/mips_sim.c
+++ test_server/mips_sim.c
@@ -9,7 +9,11 @@ long long cycles;
 
 // registers
 int regs[32];
-int * return_address;
+int rs, rt;
+
+int rd, shamt, funct; //for R-format
+int add; //for I-format
+int jump_address; //for J-format
 
 // program counter
 int pc;
@@ -21,7 +25,7 @@ int inst_mem[INST_MEM_SIZE]; //instruction memory
 int data_mem[DATA_MEM_SIZE]; //data memory
 
 //Control Unit
-typedef struct Execution{
+typedef struct Execution {
 	char Regdst;
 	char ALUOp1;
 	char ALUOp0;
@@ -34,12 +38,12 @@ typedef struct Memory {
 	char MemWrite;
 } Memory;
 
-typedef struct WriteBack{
+typedef struct WriteBack {
 	char RegWrite;
 	char MemtoReg;
 } WriteBack;
 
-typedef struct Control_Unit{
+typedef struct Control_Unit {
 	Execution EX;
 	Memory M;
 	WriteBack WB;
@@ -57,34 +61,36 @@ void decode();
 
 
 //sub_function
-void make_decimal(int * decimal, int start, int size); //decode instruction to decimal number
+void make_decimal(int* decimal, int start, int size); //decode instruction to decimal number
 
 //main
 int main()
 {
-	char done=FALSE;
+	char done = FALSE;
 
 	init();
-	while(!done)
+	while (!done)
 	{
 		fetch();     //fetch an instruction from a instruction memory
 		decode();    //decode the instruction and read data from register file
-		/*exe();       //perform the appropriate operation 
+		/*exe();       //perform the appropriate operation
 		mem();       //access the data memory
 		wb();        //write result of arithmetic operation or data read from the data memory if required
 		*/
 		cycles++;    //increase clock cycle
-		
+
+		rs = rt = rd = shamt = funct = add = jump_address = 0; //reset every registers
+
 		// check the exit condition 
-		if(regs[9]==10)  //if value in $t1 is 10, finish the simulation
-			done=TRUE;
+		if (regs[9] == 10)  //if value in $t1 is 10, finish the simulation
+			done = TRUE;
 
 		//if debug mode, print clock cycle, pc, reg 
 	}
 
 	/*print_cycles();  //print clock cycles
-	print_pc();		 //print pc
-	print_reg();	 //print registers
+	print_pc();       //print pc
+	print_reg();    //print registers
 	*/
 	return 0;
 }
@@ -96,32 +102,32 @@ int main()
 
 void init()
 {
-	FILE* fp = fopen("runme.hex","r");
+	FILE* fp = fopen("runme.hex", "r");
 	int i;
 	long inst;
 
-	if(fp == NULL)
+	if (fp == NULL)
 	{
-		fprintf(stderr,"Error opening file.\n");
+		fprintf(stderr, "Error opening file.\n");
 		exit(2);
 	}
 
 	/* fill instruction memory */
-	i=0;
-	while(fscanf(fp, "%x", &inst)==1)
+	i = 0;
+	while (fscanf(fp, "%x", &inst) == 1)
 	{
-		inst_mem[i++]=inst;
+		inst_mem[i++] = inst;
 	}
-	
-	
+
+
 	/*reset the registers*/
-	for(i=0;i<32;i++)
+	for (i = 0; i < 32; i++)
 	{
-		regs[i]=0;
+		regs[i] = 0;
 	}
 
 	/*reset pc*/
-	pc=0;
+	pc = 0;
 }
 
 
@@ -143,7 +149,7 @@ void fetch() //read address and make it bit
 
 void decode()
 {
-	int opcode = 0, jump_address = 0;
+	int opcode = 0, pc_for_concat;
 
 	make_decimal(&opcode, 0, 6); //decode opcode
 
@@ -162,7 +168,7 @@ void decode()
 		control.jump = 0;
 		control.link = 0;
 		break;
-	case 2: //Jump
+	case 2: //Jump, j
 		control.EX.Regdst = 'x';
 		control.EX.ALUOp1 = 'x';
 		control.EX.ALUOp0 = 'x';
@@ -173,9 +179,9 @@ void decode()
 		control.WB.RegWrite = 0;
 		control.WB.MemtoReg = 'x';
 		control.jump = 1;
-		control.link = 0;
+		control.link = 0;	
 		break;
-	case 3: //Jal
+	case 3: //Jal, j
 		control.EX.Regdst = 'x';
 		control.EX.ALUOp1 = 'x';
 		control.EX.ALUOp0 = 'x';
@@ -188,7 +194,7 @@ void decode()
 		control.jump = 1;
 		control.link = 1;
 		break;
-	case 4: //beq
+	case 4: //beq, i
 		control.EX.Regdst = 'x';
 		control.EX.ALUOp1 = 0;
 		control.EX.ALUOp0 = 1;
@@ -201,7 +207,7 @@ void decode()
 		control.jump = 0;
 		control.link = 0;
 		break;
-	case 8: //add immediate
+	case 8: //add immediate, i
 		control.EX.Regdst = 0;
 		control.EX.ALUOp1 = 0;
 		control.EX.ALUOp0 = 0;
@@ -214,7 +220,7 @@ void decode()
 		control.jump = 0;
 		control.link = 0;
 		break;
-	case 10: //set less than immediate
+	case 10: //set less than immediate, i
 		control.EX.Regdst = 0;
 		control.EX.ALUOp1 = 0;
 		control.EX.ALUOp0 = 0;
@@ -227,7 +233,7 @@ void decode()
 		control.jump = 0;
 		control.link = 0;
 		break;
-	case 35: //load word
+	case 35: //load word, i
 		control.EX.Regdst = 0;
 		control.EX.ALUOp1 = 0;
 		control.EX.ALUOp0 = 0;
@@ -240,7 +246,7 @@ void decode()
 		control.jump = 0;
 		control.link = 0;
 		break;
-	case 43: //store word
+	case 43: //store word, i
 		control.EX.Regdst = 'x';
 		control.EX.ALUOp1 = 0;
 		control.EX.ALUOp0 = 0;
@@ -255,31 +261,61 @@ void decode()
 		break;
 	}
 
-	if (control.jump && !(control.link)) //jump execution
+	/*calculating address*/
+	if (control.jump == 1) //for J-format
 	{
 		make_decimal(&jump_address, 6, 26); //decode jump address
-		jump_address << 2;
+		pc_for_concat = (pc & 4026531840); //copy MSB 4bit
+		jump_address += pc_for_concat; //concatenate
+	}
+	else
+	{
+		make_decimal(&rs, 6, 5); //decode rs
+		make_decimal(&rt, 11, 5); //decode rt
+
+		if (control.EX.Regdst == 1) //for R-format
+		{
+			make_decimal(&rd, 16, 5); //decode rd
+			make_decimal(&shamt, 21, 5); //decode shift amount
+			make_decimal(&funct, 26, 6); //decode function
+		}
+		else
+		{
+			if (regs[16]) //negative number, sign-extend
+			{
+				for (int i = 16; i <= 31; i++) //make positive number - 1
+				{
+					regs[i] = !regs[i];
+				}
+				make_decimal(&add, 16, 16); //decode address
+				add++; //recover lost 1
+				add *= -1; //make negative number
+			}
+			else //positive number
+			{
+				make_decimal(&add, 16, 16); //decode address
+			}
+			
+		}
 	}
 
-
+	return;
 }
 
-void make_decimal(int * decimal, int start, int size) //decode instruction to decimal number
+void make_decimal(int *decimal, int start, int size) //decode instruction to decimal number
 {
 	int i;
 
-	for (i = start; i < size; i++)
+	for (i = start; i < start + size; i++)
 	{
-		for (int i = 0; i < 6; i++)
+		if (regs[i])
 		{
-			if (regs[i])
-			{
-				(*decimal)++;
-			}
-			(*decimal) *= 2;
+			(*decimal)++;
 		}
-
+		(*decimal) = (*decimal) << 1;
 	}
+
+	(*decimal)  = (*decimal) >> 1;
 
 	return;
 }
